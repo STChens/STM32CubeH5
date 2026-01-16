7461989e3d1885d2837d7465a8d6d76c41c88f10
Sunny CHEN <sunny.chen@st.com>
2026-01-16 15:44:40 +0800

    Update ob_flash_programming script

--- Projects/NUCLEO-H563ZI/ROT_Provisioning/OEMiROT/ob_flash_programming.bat
+++ Projects/NUCLEO-H563ZI/ROT_Provisioning/OEMiROT/ob_flash_programming.bat
@@ -1,24 +1,26 @@
 call ../env.bat
-set cube_fw_path=%cube_fw_path:"=%
+:: Get config updated by OEMiROT_Boot
+call img_config.bat
 set rot_provisioning_path=%rot_provisioning_path:"=%
+set cube_fw_path=%cube_fw_path:"=%
 
 :: Enable delayed expansion
 setlocal EnableDelayedExpansion
 
 set sec1_start=0
-set sec1_end=0xE
+set sec1_end=0x76
 set sec2_start=0x7F
 set sec2_end=0x0
 set wrpgrp1=0xFFFFFFF8
 set wrpgrp2=0xFFFFFFFF
 set hdp1_start=0
-set hdp1_end=0xB
+set hdp1_end=0x13
 set hdp2_start=0x7F
 set hdp2_end=0x0
 set boot_lck=0xB4
 set bootaddress=0xC000000
 set bootob=0xC0000
-set app_image_number=2
+set app_image_number=1
 set s_data_image_number=0
 set ns_data_image_number=0
 
@@ -38,7 +40,7 @@ set appli_dir=../../%oemirot_appli_path_project%
 )
 
 :: =============================================== Remove protections and initialize Option Bytes ==========================================
-set remove_protect_init=-ob SECWM1_STRT=1 SECWM1_END=0 WRPSGn1=0xffffffff WRPSGn2=0xffffffff SECWM2_STRT=1 SECWM2_END=0 HDP1_STRT=1 HDP1_END=0 HDP2_STRT=1 HDP2_END=0 SECBOOT_LOCK=0xC3 SECBOOTADD=%bootob% SWAP_BANK=0 SRAM2_RST=0 SRAM2_ECC=0 SRAM3_ECC=1 BOOT_UBE=0xB4
+set remove_protect_init=-ob SECWM1_STRT=1 SECWM1_END=0 WRPSGn1=0xffffffff WRPSGn2=0xffffffff SECWM2_STRT=1 SECWM2_END=0 HDP1_STRT=1 HDP1_END=0 HDP2_STRT=1 HDP2_END=0 SECBOOT_LOCK=0xC3 SWAP_BANK=0 SRAM2_RST=0 SRAM2_ECC=0 BOOT_UBE=0xB4
 
 :: =============================================== Erase the user flash =====================================================================
 set erase_all=-e all
@@ -47,7 +49,7 @@ set erase_all=-e all
 set hide_protect=HDP1_STRT=%hdp1_start% HDP1_END=%hdp1_end% HDP2_STRT=%hdp2_start% HDP2_END=%hdp2_end%
 set write_protect=WRPSGn1=%wrpgrp1% WRPSGn2=%wrpgrp2%
 set sec_water_mark=SECWM1_STRT=%sec1_start% SECWM1_END=%sec1_end% SECWM2_STRT=%sec2_start% SECWM2_END=%sec2_end%
-set boot_lock=-ob SECBOOT_LOCK=%boot_lck%
+set boot_lock=SECBOOT_LOCK=%boot_lck%
 
 :: =============================================== Configure Option Bytes ====================================================================
 set "action=Set TZEN = 1"
@@ -60,6 +62,12 @@ set "action=Remove Protection and erase All"
 echo %action%
 %stm32programmercli% %connect_reset%
 %stm32programmercli% %connect_reset% %remove_protect_init% %erase_all%
+IF !errorlevel! NEQ 0 goto :error
+
+set "action=Set SecureBoot address"
+echo %action%
+%stm32programmercli% %connect_reset%
+%stm32programmercli% %connect_reset% -ob SECBOOTADD=%bootob%
 IF !errorlevel! NEQ 0 goto :error
 
 set "action=Configure Secure Water Mark"
@@ -84,12 +92,20 @@ echo "TZ Appli NonSecure Written"
 )
 
 if  "%app_image_number%" == "1" (
+if  "%app_full_secure%" == "1" (
+set "action=Write Appli Full Secure"
+echo %action%
+%stm32programmercli% %connect_no_reset% -d %appli_dir%\Binary\%s_code_image% -v
+IF !errorlevel! NEQ 0 goto :error
+echo "Appli Full Secure Written"
+) else (
 set "action=Write One image Appli"
 echo %action%
 %stm32programmercli% %connect_no_reset% -d %appli_dir%\Binary\%one_code_image% -v
 IF !errorlevel! NEQ 0 goto :error
 
 echo "TZ Appli Written"
+)
 )
 
 if  "%s_data_image_number%" == "1" (
