d5e76606c8a37bb89046e7c13149aa93ee5dbe81
Sunny CHEN <sunny.chen@st.com>
2026-02-02 14:43:25 +0800

    OEMiROT (NTZ): Update loader files

--- Projects/NUCLEO-H563ZI/Applications/ROT_NTZ/OEMiROT_Loader/Inc/main.h
+++ Projects/NUCLEO-H563ZI/Applications/ROT_NTZ/OEMiROT_Loader/Inc/main.h
@@ -32,10 +32,10 @@ extern "C" {
 
 /* Private define ------------------------------------------------------------*/
 #define APP_CODE_OFFSET      0x2A000 /* This define is updated automatically from ROT_BOOT project */
-#define APP_CODE_SIZE        0xC6000 /* This define is updated automatically from ROT_BOOT project */
+#define APP_CODE_SIZE        0xE6000 /* This define is updated automatically from ROT_BOOT project */
 #define IMAGE_HEADER_SIZE  (0x400)  /* mcuboot headre size */
 
-#define LOADER_CODE_START       0x08028000
+#define LOADER_CODE_START       0x81F8000
 
 /* Exported constants --------------------------------------------------------*/
 /* Exported types ------------------------------------------------------------*/
