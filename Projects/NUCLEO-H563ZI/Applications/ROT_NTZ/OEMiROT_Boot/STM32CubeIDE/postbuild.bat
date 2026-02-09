4210de0c053320a774fc32b7b2fa01066db4b794
Sunny CHEN <sunny.chen@st.com>
2026-02-09 18:26:52 +0800

    OEMiROT (NTZ): Fix boot postbuild issue for dual slot swap case

--- Projects/NUCLEO-H563ZI/Applications/ROT_NTZ/OEMiROT_Boot/STM32CubeIDE/postbuild.bat
+++ Projects/NUCLEO-H563ZI/Applications/ROT_NTZ/OEMiROT_Boot/STM32CubeIDE/postbuild.bat
@@ -265,9 +265,9 @@ set "command=%python%%applicfg% xmlval --layout %preprocess_bl2_file% -m RE_FLAS
 %command%
 IF !errorlevel! NEQ 0 goto :error
 
-::set "command=%python%%applicfg% xmlval -xml %app_code_xml% -nxml %code_size% -nxml %scratch_sector_number% --decimal -e (((val1+1)/val2)+1) -cond val2 -c M %app_code_xml% --vb >> %current_log_file% 2>&1"
-::%command%
-::IF !errorlevel! NEQ 0 goto :error
+set "command=%python%%applicfg% xmlval -xml %app_code_xml% -nxml %code_size% -nxml %scratch_sector_number% --decimal -e (((val1+1)/val2)+1) -cond val2 -c M %app_code_xml% --vb >> %current_log_file% 2>&1"
+%command%
+IF !errorlevel! NEQ 0 goto :error
 
 :: ============================================================ Update %app_data_init_xml% ==============================================================
 set "command=%python%%applicfg% xmlname -n %firmware_execution_offset% -c x %app_data_init_xml% --vb >> %current_log_file% 2>&1"
@@ -303,9 +303,9 @@ set "command=%python%%applicfg% xmlparam --layout  %preprocess_bl2_file% -m RE_P
 %command%
 IF !errorlevel! NEQ 0 goto :error
 
-::set "command=%python%%applicfg% xmlval --layout %preprocess_bl2_file% -m RE_FLASH_AREA_SCRATCH_SIZE -n %scratch_sector_number% --decimal %app_data_xml% --vb >> %current_log_file% 2>&1"
-::%command%
-::IF !errorlevel! NEQ 0 goto :error
+set "command=%python%%applicfg% xmlval --layout %preprocess_bl2_file% -m RE_FLASH_AREA_SCRATCH_SIZE -n %scratch_sector_number% --decimal %app_data_xml% --vb >> %current_log_file% 2>&1"
+%command%
+IF !errorlevel! NEQ 0 goto :error
 
 set "command=%python%%applicfg% xmlval -xml %app_data_xml% -nxml %data_size% -nxml %scratch_sector_number% --decimal -e (((val1+1)/val2)+1) -cond val2 -c M %app_data_xml% --vb >> %current_log_file% 2>&1"
 %command%
